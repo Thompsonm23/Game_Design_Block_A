dc5432fd297ddc545f5c6c002bd38d72cd6ed156
Thompsonm23 <thompsonm2314@gmail.com>
2021-09-14 22:09:59 -0500

    Homework on strings

---
+++ Hw.py
@@ -0,0 +1,25 @@
+#Maurice Thompson
+#This is my homework for string
+import os
+os.system('cls')
+
+print("hello")
+#Slice
+b = "Hello, World!"
+print(b[2:5])
+#Modify
+a = "Hello, World!"
+print(a.upper())
+#String Connection
+c = "Hello"
+d = "World"
+e = c + d
+print(e)
+#String Format
+age = 36
+txt = "My name is John, and I am "
+print(txt.format(age))
+print(txt)
+#Escape Character
+bal = "We are the so-called \"Vikings\" from the north."
+print(bal)
