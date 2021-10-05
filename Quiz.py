0161e8d4240f8c1e5b8e88133a1d455d483a9d4e
Thompsonm23 <thompsonm2314@gmail.com>
2021-10-05 09:16:22 -0500

    quiz

---
+++ Quiz.py
@@ -0,0 +1,29 @@
+#Maurice Thompson
+import os
+os.system('cls')
+
+#Question 1
+print(len('Hellow world!'))
+#Question 2
+strString = 'Study.com is a wonderful resource.'
+print(strString.find('great'))
+print(strString.find('wonderful'))
+print(strString.find('.'))
+#Question 3
+print(strString[15:24])
+#Question 4
+print(strString[15:24].find('der')+len(strString))
+#Question 6
+var1= 17
+var2= 4
+result = var1%var2
+print(result)
+#Question 7
+
+#Question 8
+
+#Question 9
+
+#Question 10
+MyList='0','2','3','4','6','7','8','9','10'
+MyList.insert(5)='5'
